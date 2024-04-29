208e9299367403d6b8dd2ba7164254554d8ee665
A-Dev <3xasov@gmail.com>
2024-04-29 17:51:27 +0800

    zxc

--- app/Livewire/Accommodations.php
+++ app/Livewire/Accommodations.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Accommodation;
+use App\Models\Page;
 use Livewire\Component;
 
 class Accommodations extends Component
 {
     public function render()
     {
-        return view('livewire.accommodations');
+        return view('livewire.accommodation', [
+            'page' => Page::where('status', '1')->where('id', '3')->first(),
+            'data' => Accommodation::where('status', '1')->get(),
+        ]);
     }
 }
